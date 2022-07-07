9ee10350f9d0822cdb82e8ab98e0c7100ef6eee8
Ganji Sushma Reddy <108218994+GanjiSushmaReddy@users.noreply.github.com>
2022-07-07 14:31:52 +0530

    Add files via upload

---
+++ MCQ-quiz_game.py
@@ -0,0 +1,109 @@
+import time
+
+#Welcome the user
+print("Welcome to the Simple Quiz Game!")
+time.sleep(1)
+
+#Chances
+chances = 1
+print("You will have", chances, "chance to answer correctly. \nPlease put the alphabet of the answer\n")
+time.sleep(2)
+
+#Score
+score = 0
+
+#question number 1
+question_1 = print("1) How many days do we have in a week?\n(a) 5 \n(b) 2\n(c) 6\n(d) 7\n\n ")
+answer_1 = "d"
+
+for i in range(chances):
+    answer = input("Answer: ")
+    if (answer.lower() == answer_1):
+        print("Correct! Good Job.\n")
+        score = score + 1
+        break
+    else:
+        print("Incorrect!\n")
+        time.sleep(0.5)
+        print("The correct answer is", answer_1, "\n\n")
+
+time.sleep(2)
+
+#question number 2
+question_2 = print("2) How many colors are there in a rainbow?\n(a) 5\n(b) 2\n(c) 6\n(d) 7\n\n ")
+answer_2 = "d"
+
+for i in range(chances):
+    answer = input("Answer: ")
+    if (answer.lower() == answer_2):
+        print("Correct! Good Job.\n")
+        score = score + 1
+        break
+    else:
+        print("Incorrect!\n")
+        time.sleep(0.5)
+        print("The correct answer is", answer_2, "\n\n")
+
+time.sleep(2)
+
+#question number 3
+question_3 = print("3) How many letters are there in the English alphabet?\n(a) 23\n(b) 25\n(c) 26\n(d) 27\n\n ")
+answer_3 = "c"
+
+for i in range(chances):
+    answer = input("Answer: ")
+    if (answer.lower() == answer_3):
+        print("Correct! Good Job.\n")
+        score = score + 1
+        break
+    else:
+        print("Incorrect!\n")
+        time.sleep(0.5)
+        print("The correct answer is", answer_3, "\n\n")
+
+time.sleep(2)
+
+#question number 4
+question_4 = print("4)  How many sides are there in a triangle?\n(a) 5\n(b) 3\n(c) 4\n(d) 6\n\n ")
+answer_4 = "b"
+
+for i in range(chances):
+    answer = input("Answer: ")
+    if (answer.lower() == answer_4):
+        print("Correct! Good Job.\n")
+        score = score + 1
+        break
+    else:
+        print("Incorrect!\n")
+        time.sleep(0.5)
+        print("The correct answer is", answer_4, "\n\n")
+
+time.sleep(2)
+
+#question number 5
+question_5 = print("5) In which direction does the sun rise?\n(a) East\n(b) West\n(c) North\n(d) South\n\n ")
+answer_5 = "a"
+
+for i in range(chances):
+    answer = input("Answer: ")
+    if (answer.lower() == answer_5):
+        print("Correct! Good Job.\n")
+        score = score + 1
+        break
+    else:
+        print("Incorrect!\n")
+        time.sleep(0.5)
+        print("The correct answer is", answer_5, "\n\n")
+
+time.sleep(1)
+
+#print the score
+while score >= 4:
+    print("Well done! Your score was", score)
+    break
+
+while score <= 3:
+    print("Better luck next time! Your score was", score)
+    break
+#Goodbye message
+print("Thank you for playing the Simple Quiz Game!")
